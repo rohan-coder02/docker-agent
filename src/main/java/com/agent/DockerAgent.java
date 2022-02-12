db4f84416d87bb30a2f40492b90092849d8bf927
rohan-coder02 <30956097+rohan-coder02@users.noreply.github.com>
2022-02-12 15:05:26 +0530

    Update DockerAgent.java
    currently using thread.sleep to sample data every 60 seconds

--- src/main/java/com/agent/DockerAgent.java
+++ src/main/java/com/agent/DockerAgent.java
@@ -21,7 +21,6 @@ public class DockerAgent {
     private static final CpuMetrics cpuMetrics = new CpuMetrics();
     private static final MemoryMetrics memoryMetrics = new MemoryMetrics();
     private static final NetworkMetrics networkMetrics = new NetworkMetrics();
-    private static String scheduler[] = {"Container-Scheduler", "Cpu-Scheduler", "Memory-Scheduler", "Network-Scheduler"};
 
     public static void main(String[] args) throws InterruptedException {
         initialize();
@@ -51,20 +50,5 @@ public class DockerAgent {
 
     private static void collectSamples(long time) throws InterruptedException {
         Thread.sleep(time);
-    }
-
-    private static void startMetricCollection(){
-        Timer timer = new Timer(scheduler[0]);
-        TimerTask containerTask = new ContainerData();
-        timer.schedule(containerTask, 0, 60000);
-        Timer timer1 = new Timer(scheduler[1]);
-        TimerTask cpuTask = new CpuMetrics();
-        timer1.schedule(cpuTask, 0, 60000);
-        Timer timer2 = new Timer(scheduler[2]);
-        TimerTask memoryTask = new MemoryMetrics();
-        timer2.schedule(memoryTask, 0, 60000);
-        Timer timer3 = new Timer(scheduler[3]);
-        TimerTask networkTask = new NetworkMetrics();
-        timer3.schedule(networkTask, 0, 60000);
     }
 }
